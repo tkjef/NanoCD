e51c7eca1d429d41a28d64ea2092db6c40b8bfb4
Jef Yo <yo@tkjef.com>
2018-12-22 14:12:32 -0800

    udpdating path on test

--- tests/test3.sh
+++ tests/test3.sh
@@ -2,12 +2,12 @@
 
 printf "\n\n"
 echo "STARTING NANOCD TEST ON TEST REPO"
-printf "\n\n"
+printf "\n"
 
 DATE=`date +"%Y-%m-%d-%H-%M"`
 FEATURE_BRANCH=$(git branch 2>/dev/null | grep '^*' | colrm 1 2)
 
-cp nanocd /usr/local/bin/nanocd
+cp ../nanocd /usr/local/bin/nanocd
 cd ~/bash_test/
 git checkout -b $DATE
 touch ${DATE}_test.txt
