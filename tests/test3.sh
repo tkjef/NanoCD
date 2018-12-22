62094bcda646ebfd5658690db4f868ec5bd07bfe
Jef Yo <yo@tkjef.com>
2018-12-22 13:56:39 -0800

    add .gitignore and test3

--- tests/test3.sh
+++ tests/test3.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+DATE=`date +"%Y-%m-%d-%H-%M"`
 cp nanocd /usr/local/bin/nanocd
+cd ~/projects/bash_test/
+git checkout -b $DATE
+touch ${DATE}_test.txt
+git add .
+git add *
+git commit -m $DATE
+git push
